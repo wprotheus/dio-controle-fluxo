b5b873e2a7aa12fcc43120c4c8b4514548ecc63c
wprotheus <wellneto@hotmail.com>
2025-03-09 08:47:02 -0300

    feat: Controle de fluxo.

---
+++ src/main/java/ResultadoEscolar.java
@@ -0,0 +1,22 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+//        int nota = 6;
+//
+//        if (nota >= 7)
+//            System.out.println("Aprovado");
+//        else if (nota >= 5 && nota < 7)
+//            System.out.println("Recuperação");
+//        else
+//            System.out.println("Reprovado");
+
+//        int nota = 7;
+//        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+//        System.out.println(resultado);
+
+        int nota = 6;
+        String resultado = nota >= 7 ?
+                "Aprovado" : nota >= 5 && nota < 7 ?
+                "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
